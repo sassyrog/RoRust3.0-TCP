329fa8b4978673f8a737cc48cac0c26ef21fd2e0
Sassyrog <rogerndaba@gmail.com>
2024-09-19 21:40:36 +0000

    🚧 still starting my TCP implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> anyhow::Result<()> {
+    let tun = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
+    let mut buffer: [u8; 1504] = [0; 1504];
+    loop {
+        let nbytes = tun.recv(&mut buffer)?;
+        eprintln!("Read {} bytes", nbytes);
+    }
+    // Ok(())
 }
